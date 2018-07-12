cc47b638a655b72940f3c0079a1a9de7b8ec45f1
Figamus <fig74656@yahoo.com>
2018-07-12 14:17:39 -0500

    Added Forms

--- js/main.js
+++ js/main.js
@@ -15,3 +15,5 @@ mainCharacter.age = 35
 mainCharacter.race = characterRace
  console.log(`${mainCharacter.name}, ${mainCharacter.age}, ${mainCharacter.race}`)
 document.getElementById("actionScreen").innerHTML = `<p>Greetings Traveler. Your name is ${mainCharacter.name}. You tell me you are ${mainCharacter.age} year of age and that you are ${mainCharacter.race}</p>`;
+
+// Turn the form data into string with a .value at the end.
