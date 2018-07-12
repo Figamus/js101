1acd0f2ca24bc964d79b53479bff10e00ef1f014
Figamus <fig74656@yahoo.com>
2018-07-12 13:10:55 -0500

    Added basic questions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 console.log("hello main.js")
 
-let races = ["Human", "Elf", "Dwarf", "Halfling", "Draconin", "Wolfen", "Katar"]
 let mainCharacter = {
     name: "",
     age: 0,
     race: "",
 }
-
+window.onload = console.log("Loaded")
 var characterName = prompt("What is your name Adveturer?")
 var characterAge = prompt("How old are you, " + characterName)
 var characterRace = prompt(characterName + ", I don't recognize your species. Could you tell me?")
@@ -14,6 +13,5 @@ var characterRace = prompt(characterName + ", I don't recognize your species. Co
 mainCharacter.name = characterName
 mainCharacter.age = 35
 mainCharacter.race = characterRace
-
-
-console.log(mainCharacter.name, mainCharacter.age, mainCharacter.race)
+ console.log(`${mainCharacter.name}, ${mainCharacter.age}, ${mainCharacter.race}`)
+document.getElementById("actionScreen").innerHTML = `<p>Greetings Traveler. Your name is ${mainCharacter.name}. You tell me you are ${mainCharacter.age} year of age and that you are ${mainCharacter.race}</p>`;
