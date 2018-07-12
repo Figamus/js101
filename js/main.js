7da7492090b8cdd53182578c53c501ca8ce1c1ed
Figamus <fig74656@yahoo.com>
2018-07-12 08:59:51 -0500

    Added basic prompt and implementation of prompt's into future strings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 console.log("hello main.js")
 
-let lostColors = {
-    yellow: ["dandelion", "mac and cheese"]
+let races = ["Human", "Elf", "Dwarf", "Halfling", "Draconin", "Wolfen", "Katar"]
+let mainCharacter = {
+    name: "",
+    age: 0,
+    race: "",
 }
+
+var characterName = prompt("What is your name Adveturer?")
+var characterAge = prompt("How old are you, " + characterName)
+var characterRace = prompt(characterName + ", I don't recognize your species. Could you tell me?")
+
+mainCharacter.name = characterName
+mainCharacter.age = 35
+mainCharacter.race = characterRace
+
+
+console.log(mainCharacter.name, mainCharacter.age, mainCharacter.race)
